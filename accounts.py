22ea9b6f7c453831c922f80a017cecc3c9fc735d
BuildTools <unconfigured@null.spigotmc.org>
2023-10-18 15:26:39 +0100

    Remove Passwords

--- accounts.py
+++ accounts.py
@@ -3,7 +3,7 @@ class Account:
         self.email = email
         self.password = password
 
-alice = Account("alice.tester17@gmail.com", "8HzbIc7BG5T08kvOCdoe")
-bob = Account("bob.tester131@gmail.com", "ZAGa8hURRFv9aDMJBXRF")
-carol = Account("carol.tester4@gmail.com", "TZMRqbYxif0IvZqYzqFd")
-dave = Account("dave.tester13@gmail.com", "cuLy75xwl0AKMlPOkpO0")
+alice = Account("alice.tester17@gmail.com", "REDACTED")
+bob = Account("bob.tester131@gmail.com", "REDACTED")
+carol = Account("carol.tester4@gmail.com", "REDACTED")
+dave = Account("dave.tester13@gmail.com", "REDACTED")
